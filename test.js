1d283050582f6f1be5058c831465a279f849bcb4
suraj pal <suraj8.pal@ril.com>
2026-07-05 12:28:19 +0530

    latest

--- test.js
+++ test.js
@@ -1,7 +1,11 @@
 const fs = require("fs");
+const EventEmitter = require("events");
+
 
 console.log(process.argv)
-const stream = fs.createReadStream("./automator-mobile-agent.txt", {
+
+//readable stream with highWaterMark 
+const stream = fs.createReadStream("./code-snippet.txt", {
     highWaterMark: 1024 // chunk size in bytes
 });
 var i = 0
@@ -9,3 +13,15 @@ stream.on("data", (chunk) => {
     console.log("Received chunk:", ++i);
 });
 console.log("stream: ", i);
+
+
+//event emitter
+const emitter =new EventEmitter();
+//listener
+emitter.on("greet", (name) => {
+    console.log(`Hello ${name}`);
+});
+//emitter
+emitter.emit("greet", "Suraj");
+
+
